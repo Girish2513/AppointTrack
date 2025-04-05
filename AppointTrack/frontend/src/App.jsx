839c91f0ffb69bf9e9bad6ead527e679af179506
Girish2513 <girishsaana2513@gmail.com>
2025-04-05 22:12:40 +0530

    Fix Vercel build issue with vite permissions

--- AppointTrack/frontend/src/App.jsx
+++ AppointTrack/frontend/src/App.jsx
@@ -1,11 +1,4 @@
 
-// export default function App() {
-//   return (
-//     <div className="flex justify-center items-center h-screen bg-gray-100">
-//       <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind + React!</h1>
-//     </div>
-//   );
-// }
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
